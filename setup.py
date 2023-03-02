822e710bb4955672ed0f2274b75bf53b77ee5dac
vangheem <vangheem@gmail.com>
2023-03-02 10:19:20 -0500

    missing man

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if system() != "Windows":
 
 setup(
     name="memorylru",
-    version="1.0.3",
+    version="1.0.4",
     long_description=README,
     long_description_content_type="text/markdown",
     classifiers=[
